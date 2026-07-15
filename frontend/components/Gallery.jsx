b1cd5a590e11e3fc7a9a3cae1807c79a50dcb4fe
Faruque Abdullah <abdullahfaruque007@gmail.com>
2026-07-15 12:08:26 +0530

    add revalidate to fix caching

--- frontend/components/Gallery.jsx
+++ frontend/components/Gallery.jsx
@@ -3,31 +3,33 @@ import { galleryQuery } from "@/lib/queries";
 import { urlFor } from "@/lib/imageUrl";
 import Image from "next/image";
 
-export const dynamic = "force-dynamic";
-
 export default async function Gallery() {
-  const images = await client.fetch(galleryQuery);
+  const images = await client.fetch(
+    galleryQuery,
+    {},
+    { next: { revalidate: 60 } },
+  );
 
   // console.log(images);
 
   return (
     <>
-    <h1>Total images : {images.length}</h1>
+      <h1>Total images : {images.length}</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-10">
-      {images.map((item) => (
-        <div key={item._id}>
-          <Image
-            src={urlFor(item.image).url()}
-            alt={item.alt || item.title}
-            width={500}
-            height={500}
-            unoptimized
-          />
+        {images.map((item) => (
+          <div key={item._id}>
+            <Image
+              src={urlFor(item.image).url()}
+              alt={item.alt || item.title}
+              width={500}
+              height={500}
+              unoptimized
+            />
 
-          <h2 className="mt-2 font-semibold">{item.title}</h2>
-        </div>
-      ))}
-    </div>
+            <h2 className="mt-2 font-semibold">{item.title}</h2>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
